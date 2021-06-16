bc3befdabda04a0a612c8b910879fdb9dc5894b1
shruti <shrutisrivastava63999@gmail.com>
2021-06-16 18:16:14 +0530

    Updates

---
+++ precache-manifest.ef19c1b5ed5d31043919360feb6203a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ea3ddce861f2d7914cc235df651e42",
+    "url": "/me_news_app/index.html"
+  },
+  {
+    "revision": "8aabc0abe7126c792f2d",
+    "url": "/me_news_app/static/css/main.293ce728.chunk.css"
+  },
+  {
+    "revision": "150c51173970bb91451b",
+    "url": "/me_news_app/static/js/2.61d4c87a.chunk.js"
+  },
+  {
+    "revision": "6ef57ea73f859e79d7f696b27898a6e5",
+    "url": "/me_news_app/static/js/2.61d4c87a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aabc0abe7126c792f2d",
+    "url": "/me_news_app/static/js/main.219c3b4d.chunk.js"
+  },
+  {
+    "revision": "a8926d08b47d0e484e85",
+    "url": "/me_news_app/static/js/runtime-main.8c23e303.js"
+  }
+]);
